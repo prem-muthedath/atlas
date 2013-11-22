1c064c194662da4291a5e0dd034c757525eed981
Prem Muthedath <prem.muthedath@gmail.com>
2013-11-22 20:21:03 +0530

    Renamed value to amount in costs.py

--- bom_costing/domain/model/costs.py
+++ bom_costing/domain/model/costs.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
 
 class Cost:
-	def __init__(self, value=0):
-		self.__value=value
+	def __init__(self, amount=0):
+		self.__amount=amount
 
-	def add(self, value):
-		self.__value+=value
+	def add(self, amount):
+		self.__amount+=amount
 
 	def add_to(self, part_builder):
 		part_builder.add_cost(self.__str__())
 
 	def __str__(self):
-		return str(self.__value)
+		return str(self.__amount)
 
 
 class UnitCost:
-	def __init__(self, value):
-		self.__value=value
+	def __init__(self, amount):
+		self.__amount=amount
 
 	def cost(self, cost, units):
-		cost.add(units*self.__value)
+		cost.add(units*self.__amount)
 
 	def add_to(self, part_builder):
 		part_builder.add_unit_cost(self.__data())
 
 	def __data(self):
-		return str(self.__value)
+		return str(self.__amount)
