641c5a77e86f2e13166db6d170ee65b6472368f9
Prem Muthedath <prem.muthedath@gmail.com>
2014-07-10 22:41:17 +0530

    Renamed 1 method.
    Renamed tests.run.load_test_case() as tests.

--- bom_costing/tests/run.py
+++ bom_costing/tests/run.py
@@ -9,14 +9,14 @@ from . import custom
 _LOADER = unittest.TestLoader()
 _RUNNER = unittest.TextTestRunner(verbosity=2)
 
-def load_test_case(test_case):
+def tests(test_case):
     return _LOADER.loadTestsFromTestCase(test_case)
 
 def run_all():
 	suite=unittest.TestSuite()
-	suite.addTest(load_test_case(cost.Cost))
-	suite.addTest(load_test_case(default.Default))
-	suite.addTest(load_test_case(custom.Custom))
+	suite.addTest(tests(cost.Cost))
+	suite.addTest(tests(default.Default))
+	suite.addTest(tests(custom.Custom))
 	_RUNNER.run(suite)
 
 
