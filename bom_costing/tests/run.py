36abc6c0e8f77c67ec210e43d35cb53a5cb1bcf5
Prem Muthedath <prem.muthedath@gmail.com>
2014-07-10 23:16:11 +0530

    Edited tabs.

--- bom_costing/tests/run.py
+++ bom_costing/tests/run.py
@@ -10,11 +10,11 @@ _LOADER = unittest.TestLoader()
 _RUNNER = unittest.TextTestRunner(verbosity=2)
 
 def run_all():
-	suite=unittest.TestSuite()
-	suite.addTest(tests(cost.Cost))
-	suite.addTest(tests(default.Default))
-	suite.addTest(tests(custom.Custom))
-	_RUNNER.run(suite)
+    suite=unittest.TestSuite()
+    suite.addTest(tests(cost.Cost))
+    suite.addTest(tests(default.Default))
+    suite.addTest(tests(custom.Custom))
+    _RUNNER.run(suite)
 
 def tests(test_case):
     return _LOADER.loadTestsFromTestCase(test_case)
