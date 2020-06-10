70654ef1bb3a76a8eacea31c8e5958d16f2b0683
Prem Muthedath <prem.muthedath@gmail.com>
2020-06-10 12:05:28 +0530

    1. atlas/configuration.py: `BomBuilder` -> `_BomBuilder`; 2. __main__.py: + space in `setUp()` for readability; 3. - commented code

--- atlas/components.py
+++ atlas/components.py
@@ -7,7 +7,7 @@ class Bom:
         self.__costable_pos=-1
 
     def add(self, component):
-        if component in self.__components: 
+        if component in self.__components:
             raise RuntimeError("duplicate part")
         if self.__costed_pos < 0 and component.is_costed():
             self.__costed_pos=len(self.__components)
@@ -26,7 +26,7 @@ class Bom:
     def reset(self):
         self.__costable_pos=-1
 
-    def costable(self, part): 
+    def costable(self, part):
         pos=self.__components.index(part)
         if self.__costed_pos >= 0 and pos > self.__costed_pos:
             return False
@@ -35,7 +35,7 @@ class Bom:
         if pos == len(self.__components)-1 or part.costable():
             self.__costable_pos=pos
         return self.__costable_pos==pos
-    
+
     def __str__(self):
         return "BOM: "
 
@@ -48,7 +48,7 @@ class Part:
                 cost=cost,
                 units=units
             )
-        
+
     def cost(self, bom):
         if self.is_costed() or bom.costable(self):
             return self.__cost()
@@ -62,57 +62,9 @@ class Part:
 
     def costable(self):
         return self.__attr['site'] == '1'
-    
+
     def __str__(self):
         return ", ".join([str((x, y)) for (x, y) in self.__attr.items()])
-
-# class Bom:
-#     def __init__(self):
-#         self.__components=[]
-#         self.__costed=False
-# 
-#     def add(self, component):
-#         if component in self.__components: 
-#             raise RuntimeError("duplicate part")
-#         self.__components.append(component)
-# 
-#     def cost(self, bom=None):
-#         cost=0
-#         for each in self.__components:
-#             cost=cost+each.cost(self)
-#         return cost
-# 
-#     def costed(self):
-#         self.__costed=True
-# 
-#     def costable(self, part): 
-#         if self.__costed:
-#             return False
-#         leaf = part == self.__components[-1]
-#         if leaf or part.costable():
-#             self.costed()
-#             return True
-#         return False
-# 
-# class Part:
-#     def __init__(self, number, site, cost, units):
-#         self.__attr=dict(
-#                 number=number,
-#                 site=site,
-#                 cost=cost,
-#                 units=units
-#             )
-#         
-#     def cost(self, bom):
-#         if self.__attr['site'] == '12':
-#             bom.costed()
-#             return self.__attr['units']*self.__attr['cost']
-#         if bom.costable(self):
-#             return self.__attr['units']*self.__attr['cost']
-#         return 0
-# 
-#     def costable(self):
-#         return self.__attr['site'] == '1'
 
 
 
--- atlas/configuration.py
+++ atlas/configuration.py
@@ -2,7 +2,7 @@
 
 from .components import (Bom, Part,)
 
-class BomBuilder():
+class _BomBuilder():
     def __init__(self):
         self.__parents=[Bom()]
         self.__parent_level=0
@@ -33,7 +33,7 @@ class BomBuilder():
     def __create(self):
         self.__parents.append(Bom())
         self.__parents[self.__parent_level-1].add(self.__parents[self.__parent_level])
-    
+
     def __new_parents(self):
         self.__parents=self.__parents[0:self.__parent_level+1]
 
@@ -41,74 +41,3 @@ class BomBuilder():
         return self.__parents[0]
 
 
-# costable:  P-0007
-# costed:  P-0009
-# costable:  P-0022
-# costable:  P-0023
-# costed:  P-0024
-# costed:  P-0025
-
-# class Bom:
-#     def __init__(self):
-#         self.__components=[]
-#         self.__costed=False
-# 
-#     def add(self, component):
-#         if component in self.__components: 
-#             raise RuntimeError("duplicate part")
-#         if component.is_costed():
-#             self.__costed=True
-#         self.__components.append(component)
-# 
-#     def is_costed(self):
-#         return False
-# 
-#     def cost(self, bom=None):
-#         cost=0
-#         for each in self.__components:
-#             cost=cost+each.cost(self)
-#         return cost
-# 
-#     def costed(self):
-#         self.__costed=True
-# 
-#     def costable(self, part): 
-#         if self.__costed:
-#             return False
-#         if part == self.__components[-1] or part.costable():
-#             self.costed()
-#         return self.__costed
-# 
-# class Part:
-#     def __init__(self, number, site, cost, units):
-#         self.__attr=dict(
-#                 number=number,
-#                 site=site,
-#                 cost=cost,
-#                 units=units
-#             )
-#         
-#     def cost(self, bom):
-#         print "part: ", self.__attr.items()
-#         if self.is_costed():
-#             print "costed: ", self.__attr['number']
-#             return self.__cost()
-#         if bom.costable(self):
-#             print "costable: ", self.__attr['number']
-#             return self.__cost()
-#         return 0
-#         # if self.__attr['site'] == '12':
-#         #     bom.costed()
-#         #     return self.__attr['units']*self.__attr['cost']
-#         # if bom.costable(self):
-#         #     return self.__attr['units']*self.__attr['cost']
-#         # return 0
-# 
-#     def __cost(self):
-#         return self.__attr['units']*self.__attr['cost']
-# 
-#     def is_costed(self):
-#         return self.__attr['site'] == '12'
-# 
-#     def costable(self):
-#         return self.__attr['site'] == '1'
