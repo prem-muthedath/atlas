74b89cf3a267475d8668dbfb7c2a8eb90e2dd56f
Prem Muthedath <prem.muthedath@gmail.com>
2020-10-02 20:46:07 +0530

    1. DESIGN CHANGE: report.py -- + `_TextView` to handle presentation while `_TextReport` now generates content; 2. refactored `_XmlReport`.

--- atlas/report.py
+++ atlas/report.py
@@ -11,48 +11,41 @@ class _Report(object):
     def __init__(self):
         self.__contents=[]
 
-    def _render(self, bom, schema):
+    def _render(self, bom, schema=_Schema):
         part_maps=_AtlasDB()._part_maps()
         for index, cost_map in enumerate(bom._cost_maps()):
             part_map=part_maps[index]
             part_map.update(cost_map)
-            line=[("item", index+1)] + [(i.name, part_map[i]) for i in schema]
+            line=OrderedDict([(i, part_map[i]) for i in schema])
             self.__contents.append(line)
         return self.__render()
 
     def __render(self):
-        if self.__contents==[] or len(self.__contents[0]) <= 1:
+        if len(self.__contents) == 0 or len(self._names()) == 0:
             return self._empty()
-        result = [self._title(), self._body(), self._footer()]
-        return "\n".join([i for i in result if len(i) > 0])
+        return self._render_()
 
     def _empty(self):
         return ""
 
+    def _render_(self):
+        pass
+
     def _body(self):
         body=[]
-        for line in self.__contents:
-            body.append(self._format(line))
-        return "\n".join(body)
-
-    def _title(self):
-        pass
-
-    def _footer(self):
-        pass
+        for i, line in enumerate(self.__contents):
+            body.append(self._format(i+1, line))
+        return body
 
     def _names(self):
-        if len(self.__contents) > 0:
-            return [name for (name, _) in self.__contents[0]]
-        return []
+        return self.__contents[0].keys() if len(self.__contents) > 0 else []
 
     def _totals(self):
-        fields=[i.name for i in _Schema._totals_schema() if i.name in self._names()]
+        fields=[i for i in self._names() if i in _Schema._totals_schema()]
         totals=OrderedDict()
-        lines=[dict(line) for line in self.__contents]
         for i in fields:
-            vals=[line[i] for line in lines if line.has_key(i)]
-            if i == _Schema.costed.name:
+            vals=[line[i] for line in self.__contents]
+            if i == _Schema.costed:
                 totals[i]=sum([1 if str(val) == 'Y' else 0 for val in vals])
             else:
                 totals[i]=sum(vals)
@@ -60,91 +53,90 @@ class _Report(object):
 
 ################################################################################
 
-class _TextReport(_Report):
+class _TextView():
     def __init__(self):
-        super(_TextReport, self).__init__()
         self.__field_width=15
 
-    def _format(self, line):
-        results=[]
-        for (name, value) in line:
-            if name == _Schema.level.name:
-                indent=(value-1)*"  "
-                result=indent+str(value)
-            else:
-                result=str(value)
-            results.append(self.__centered(result))
-        return " ".join(results)
+    def _render(self, title, body, footer):
+        cols=len(title)
+        tv=self.__format(title, cols)
+        bv="\n".join([" ".join([self.__centered(j) for j in i]) for i in body])
+        fv=self.__format(footer, cols)
+        return "\n".join([i for i in [tv, bv, fv] if len(i) > 0])
 
     def __centered(self, value):
         return value.center(self.__field_width)
 
+    def __format(self, vals, cols):
+        width=cols*self.__field_width + 5
+        border="-" * width
+        if len(vals) > 0:
+            data=" ".join([self.__centered(i) for i in vals])
+            return "\n".join([border, data, border])
+        return border
+
+################################################################################
+
+class _TextReport(_Report):
+    def _render_(self):
+        title=self._title()
+        body=self._body()
+        footer=self._footer()
+        return _TextView()._render(title, body, footer)
+
+    def _format(self, index, line):
+        results=[str(index)]
+        for (name, value) in line.items():
+            if name == _Schema.level:
+                indent=(value-1)*"  "
+                result=indent+str(value)
+            else:
+                result=str(value)
+            results.append(result)
+        return results
+
     def _title(self):
-        headers=[]
-        for name in self._names():
-            header=self.__centered(self.__capitalize(name))
-            headers.append(header)
-        return self.__result(headers)
+        headers=[self.__capitalize(i.name) for i in self._names()]
+        headers=[self.__capitalize("item")] + headers
+        return headers
 
     def __capitalize(self, header):
         return ' '.join(each[:1].upper()+each[1:].lower() \
                 for each in header.split('_'))
 
-    def __result(self, vals):
-        if len(vals) > 0:
-            return "\n".join([self.__border(), " ".join(vals), self.__border()])
-        return self.__border()
-
-    def __border(self):
-        width=len(self._names())*self.__field_width + 5
-        return "-" * width
-
     def _footer(self):
-        __totals=[]
         totals=self._totals()
-        if len(totals)==0: return self.__result([])
-        for name in self._names():
-            if name == "item":
-                total="Totals"
-            elif totals.has_key(name):
-                total=str(totals[name])
-            else:
-                total=" "
-            __totals.append(self.__centered(total))
-        return self.__result(__totals)
+        if len(totals) == 0: return []
+        __totals=[str(totals[name]) if totals.has_key(name) else " " for name in self._names()]
+        __totals=["Totals"] + __totals
+        return __totals
 
 ################################################################################
 
 class _XmlReport(_Report):
     def _empty(self):
-        return self.__element("xml", "")
+        return self.__element("xml", "", "")
 
-    def _body(self):
-        body=super(_XmlReport, self)._body()
-        if len(body) > 0:
-            return '\n'.join(['<parts>', body, '</parts>'])
-        return body
+    def _render_(self):
+        body="\n".join([i for i in self._body()])
+        bv=self.__element("parts", body, "\n")
+        fv=self._footer()
+        cont="\n".join([i for i in [bv, fv] if len(i) > 0])
+        return self.__element("xml", cont, "\n")
 
-    def _format(self, line):
+    def _format(self, index, line):
         fline=[]
-        for (name, value) in line:
-            if name=="item": continue
-            fline.append(self.__element(name, value))
+        for (name, value) in line.items():
+            fline.append(self.__element(name.name, value))
         return self.__element("part", "".join(fline))
 
-    def __element(self, name, val):
-        return '<' + name + '>' + str(val) + '</' + name + '>'
-
-    def _title(self):
-        return '<xml>'
+    def __element(self, name, val, sep=""):
+        return '<' + name + '>' +  sep + str(val) + sep + '</' + name + '>'
 
     def _footer(self):
         totals=self._totals()
-        val=[]
-        for name in self._names():
-            if totals.has_key(name):
-                val.append(self.__element(name, totals[name]))
-        data=self.__element("totals", "".join(val))
-        return "\n".join([data, "</xml>"]) if len (val) > 0 else "</xml>"
+        if len(totals) == 0: return ""
+        val=[self.__element(key.name, totals[key]) for key in totals]
+        return self.__element("totals", "".join(val))
 
 ################################################################################
