013a10012211a1ca6cc4e3e821436e2cc13faf78
Prem Muthedath <prem.muthedath@gmail.com>
2020-10-03 16:34:52 +0530

    report.py -- DESIGN CHANGE: (a) + `_Header`, `_Footer` -- they generate header, footer content; (b) `_Border` -> `_BorderedRow`, a decorator; (c) refactored `_TextRow`; (d) + `_TextView` to render whole view.

--- atlas/report.py
+++ atlas/report.py
@@ -24,7 +24,7 @@ class _Report(object):
         return any([i==[] for i in [self.__contents, self._names()]])
 
     def _empty(self):
-        return ""
+        pass
 
     def _render_(self):
         pass
@@ -50,51 +50,107 @@ class _Report(object):
 class _TextRow(object):
     def __init__(self, cells):
         self.__cells=cells
-        self._field_width=15
+        self.__field_width=15
 
     def _render(self):
         return " ".join([self.__centered(i) for i in self.__cells])
 
     def __centered(self, value):
-        return value.center(self._field_width)
+        return value.center(self.__field_width)
 
-    def _size(self):
-        return len(self.__cells)
+    def _width(self):
+        return len(self.__cells)*self.__field_width
 
-class _Border(_TextRow):
-    def __init__(self, cols):
-        super(_Border, self).__init__([])
-        self.__cols=cols
+class _BordereredRow(_TextRow):
+    def __init__(self, cells, row):
+        super(_BordereredRow, self).__init__(cells)
+        self.__row=row
         self.__symbol='-'
 
     def _render(self):
-        width=self.__cols*self._field_width + 5
+        data=self.__row._render()
+        if len(data) > 0:
+            return [self.__border(), data, self.__border()]
+        return [self.__border()]
+
+    def __border(self):
+        width=self._width() + 5
         return self.__symbol * width
 
 ################################################################################
 
+class _Header(object):
+    def __init__(self, tag, cols):
+        self.__tag=tag
+        self.__cols=cols
+
+    def _render(self):
+        return self._render_(self.__headers())
+
+    def _render_(self, items):
+        data=[self._capitalize()] + items
+        return self._bordered(_TextRow(data))._render()
+
+    def __headers(self):
+        return [self._capitalize(i.name) for i in self.__cols]
+
+    def _capitalize(self, word=None):
+        word=self.__tag if word==None else word
+        return ' '.join(each[:1].upper()+each[1:].lower() \
+                for each in word.split('_'))
+
+    def _bordered(self, row):
+        tag_col= 1 if self.__tag != None else 0
+        cells=[""]*(len(self.__cols) + tag_col)
+        return _BordereredRow(cells, row)
+
+    def _matches(self, cols):
+        return [col if col in cols else None for col in self.__cols]
+
+class _Footer(_Header):
+    def __init__(self, tag, cols, totals):
+        super(_Footer, self).__init__(tag, cols)
+        self.__totals=totals
+
+    def _render(self):
+        if len(self.__totals) == 0:
+            return self._bordered(_TextRow([]))._render()
+        return self._render_(self.__footers())
+
+    def __footers(self):
+        matches=self._matches(self.__totals.keys())
+        return [" " if i==None else str(self.__totals[i]) for i in matches]
+
+################################################################################
+
+class _TextView:
+    def __init__(self, header, body, footer):
+        self.__header=header
+        self.__body=body
+        self.__footer=footer
+
+    def _render(self):
+        data=self.__header._render()
+        data.extend([i._render() for i in self.__body])
+        data.extend(self.__footer._render())
+        return "\n".join(data)
+
+################################################################################
+
 class _TextReport(_Report):
-    def __init__(self):
-        super(_TextReport, self).__init__()
-        self.__captions={'ITEM' : "item", 'TOTALS' : "totals"}
+    def _empty(self):
+        return ""
 
     def _render_(self):
-        contents=self.__bordered(self.__title())
-        contents.extend(self.__body())
-        contents.extend(self.__bordered(self.__footer()))
-        return "\n".join(contents)
-
-    def __title(self):
-        headers=[self.__capitalize(i.name) for i in self._names()]
-        headers=[self.__capitalize(self.__captions['ITEM'])] + headers
-        return _TextRow(headers)._render()
-
-    def __capitalize(self, header):
-        return ' '.join(each[:1].upper()+each[1:].lower() \
-                for each in header.split('_'))
+        captions={'ITEM' : "item", 'TOTALS' : "totals"}
+        return _TextView(
+            _Header(captions['ITEM'], self._names()),
+            self.__body(),
+            _Footer(captions['TOTALS'], self._names(), self._totals())
+        )._render()
 
     def __body(self):
-        return [_TextRow(i)._render() for i in self._body()]
+        return [_TextRow(i) for i in self._body()]
 
     def _line(self, index, line):
         results=[str(index)]
@@ -106,25 +162,6 @@ class _TextReport(_Report):
                 result=str(value)
             results.append(result)
         return results
-
-    def __footer(self):
-        return _TextRow(self._footer())._render()
-
-    def _footer(self):
-        totals=self._totals()
-        if len(totals) == 0: return []
-        __totals=[str(totals[name]) if totals.has_key(name) else " " for name in self._names()]
-        return [self.__capitalize(self.__captions['TOTALS'])] + __totals
-
-    def __bordered(self, data):
-        border=self.__border()._render()
-        if len(data) > 0:
-            return [border, data, border]
-        return [border]
-
-    def __border(self):
-        cols=len(self._names()) + 1
-        return _Border(cols)
 
 ################################################################################
 
